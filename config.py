cd5bfbcc8937ed9f83c00c6e79af5a08e31e111a
bozorgzadai <bozorgzad.ai@gmail.com>
2025-03-15 17:25:56 +0330

    add db connection to config file

--- config.py
+++ config.py
@@ -5,3 +5,8 @@ host="localhost"
 user="root"
 password=""
 database="rapidremit"
+
+# host="localhost"
+# user="rapidrem_rapidremit_user"
+# password="rapidrem_rapidremit_user"
+# database="rapidrem_rapidremit_db"
